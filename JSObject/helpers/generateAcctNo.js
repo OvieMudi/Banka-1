cb3a9223ce67e0cbe89878bfe6d0a75cd23f5b26
cvjude <jjchinosoviolet@gmail.com>
2019-05-24 12:17:55 +0100

    Bug(Reload): Fix reload Issues on the UI pages
    Remove faulty lines of code thhat cause page to reload
    
    [Fixes #166235878]

--- JSObject/helpers/generateAcctNo.js
+++
@@ -1,3 +0,0 @@
-const generateAcctNo = () => Math.floor(Math.random() * 10000000000);
-
-export default generateAcctNo;
--- UI/js/app.js
+++ UI/js/app.js
@@ -1,4 +1,5 @@
-"use strict";
+
+
 const userLocation = window.location.href;
 
 const goto = (path) => {
@@ -13,60 +14,58 @@ const handleUserSingin = () => {
   const loginBtn = document.querySelector('#loginButton');
   const signinForm = document.querySelector('#signinForm');
   const errorMsg = document.querySelector('#errorMsg');
-  signinForm.addEventListener('submit', (e)=> {
+  signinForm.addEventListener('submit', (e) => {
     e.preventDefault();
     changeElementChildText(loginBtn, 'LOADING...');
-    const email = e.target.email.value,
-          password = e.target.password.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
     const url = 'https://deferral-banka-app-1.herokuapp.com/api/v1/auth/signin';
     const user = {
-      email, password
+      email, password,
     };
     fetch(url, {
       method: 'POST',
       headers: {
-        "Content-type":"application/json"
+        'Content-type': 'application/json',
       },
       body: JSON.stringify(user),
-    }).then( res => res.json())
+    }).then(res => res.json())
       .then((res) => {
-        if(res.status !== 200) {
+        if (res.status !== 200) {
           errorMsg.innerText = res.message.toUpperCase();
-          setTimeout(()=>{
-            errorMsg.innerText = ''
+          setTimeout(() => {
+            errorMsg.innerText = '';
           }, 3000);
           changeElementChildText(loginBtn, 'SIGN IN');
-        }else {
+        } else {
           window.sessionStorage.banka_token = JSON.stringify(res.data.token);
           goto('dashboard');
-          console.log(res)
+          console.log(res);
         }
-    }).catch(err => {
-      throw err;
-    })
-})
+      }).catch((err) => {
+        throw err;
+      });
+  });
 };
 
 
 const checkUserLocation = () => {
   let location = userLocation.split('/')[5];
   location = location.split('.')[0];
-  if(location !== 'sign-in'
+  if (location !== 'sign-in'
     && location !== ''
     && location !== 'index'
     && location !== 'register') {
     const token = window.sessionStorage.banka_token;
-    if(!token) {
-      goto('sign-in')
-    } else {
-      goto(location)
+    if (!token) {
+      goto('sign-in');
     }
   }
-  if(location === 'sign-in') {
+  if (location === 'sign-in') {
     handleUserSingin();
   }
 };
 
 (function startApp() {
   checkUserLocation();
-})();
+}());
